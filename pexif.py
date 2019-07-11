02a80ab0a06a0c6ad627f8a5fd3e09d2ac43617b
LorenzLamm <34575029+LorenzLamm@users.noreply.github.com>
2019-07-11 10:43:39 +0200

    Update pexif.py

--- pexif.py
+++ pexif.py
@@ -103,7 +103,7 @@ DELIM = 0xff
 EOI = 0xd9
 SOI_MARKER = b'\xff\xd8'
 #SOI_MARKER = chr(DELIM) + '\xd8'
-EOI_MARKER = b'\xd9\xd9'
+EOI_MARKER = b'\xff\xd9'
 #EOI_MARKER = chr(DELIM) + '\xd9'
 
 TIFF_OFFSET = 6
